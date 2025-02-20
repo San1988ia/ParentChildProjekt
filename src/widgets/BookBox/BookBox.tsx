c96958f56de90019bef7e321098d6d1aa68b90e1
sania <sania_19_88_@hotmail.com>
2025-02-20 09:27:09 +0100

    Gjort en enklare delete och lägg till

---
+++ src/widgets/BookBox/BookBox.tsx
@@ -0,0 +1,22 @@
+// import "./BookBox.scss";
+
+// function BookBox() {
+//   return (
+//     <>
+//       <div className="BookCard">
+//         <img
+//           src="https://bilder.akademibokhandeln.se/images_akb/9789178871032_383/zeinas-kitchen-recept-fran-mellanostern"
+//           alt=""
+//         />
+//         <p>
+//           <span>Titel:</span>Zeinas Kitchen
+//         </p>
+//         <p>
+//           <span>Författare:</span>Zeina Mourtada
+//         </p>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default BookBox;
